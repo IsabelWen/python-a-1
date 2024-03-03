1502078134fbb7131ca6686402660af9f504f1b2
Isy <isy1997@freenet.de>
2024-03-03 21:24:53 +0100

    Add practices

---
+++ Exercise 1.5/1.5-Practice Task 3/height.py
@@ -0,0 +1,77 @@
+class Height(object):
+    def __init__(self, feet, inches):
+        self.feet = feet
+        self.inches = inches
+
+    def __str__(self):
+        output = str(self.feet) + " feet, " + str(self.inches) + " inches"
+        return output
+
+    def __add__(self, other):
+        # Converting both objects' heights into inches
+        height_A_inches = self.feet * 12 + self.inches
+        height_B_inches = other.feet * 12 + other.inches
+
+        # Adding them up
+        total_height_inches = height_A_inches + height_B_inches
+
+        # Getting the output in feet
+        output_feet = total_height_inches // 12
+
+        # Getting the output in inches
+        output_inches = total_height_inches - (output_feet * 12)
+
+        # Returning the final output as a new Height object
+        return Height(output_feet, output_inches)
+    
+    def __sub__(self, other):
+        # Converting both objects' heights into inches
+        height_A_inches = self.feet * 12 + self.inches
+        height_B_inches = other.feet * 12 + other.inches
+
+        # Subtract them
+        total_height_inches = height_A_inches - height_B_inches
+
+        # Getting the output in feet
+        output_feet = total_height_inches // 12
+
+        # Getting the output in inches
+        output_inches = total_height_inches - (output_feet * 12)
+
+        # Returning the final output as a new Height object
+        return Height(output_feet, output_inches)
+    
+    # Method for less than
+    def __lt__(self, other):
+        height_inches_A = self.feet * 12 + self.inches
+        height_inches_B = other.feet * 12 + other.inches
+        return height_inches_A < height_inches_B
+
+    # Method for less than or equal
+    def __le__(self, other):
+        height_inches_A = self.feet * 12 + self.inches
+        height_inches_B = other.feet * 12 + other.inches
+        return height_inches_A <= height_inches_B
+
+    # Method for equal to
+    def __eq__(self, other):
+        height_inches_A = self.feet * 12 + self.inches
+        height_inches_B = other.feet * 12 + other.inches
+        return height_inches_A == height_inches_B
+    
+    # Method for greater than
+    def __gt__(self, other):
+        height_inches_A = self.feet * 12 + self.inches
+        height_inches_B = other.feet * 12 + other.inches
+        return height_inches_A > height_inches_B
+    
+    # Method for greather than or equal
+    def __ge__(self, other):
+        height_inches_A = self.feet * 12 + self.inches
+        height_inches_B = other.feet * 12 + other.inches
+        return height_inches_A >= height_inches_B
+    
+    # Method for not equal to
+        height_inches_A = self.feet * 12 + self.inches
+        height_inches_B = other.feet * 12 + other.inches
+        return height_inches_A != height_inches_B
